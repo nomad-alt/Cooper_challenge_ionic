bf83e9cb4205a0a1b39b154293e73ce8bd74e70f
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-27 16:47:35 +0200

    Adds login/logout buttons on the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Angular2TokenService} from "angular2-token";
+import {Component, ViewChild} from '@angular/core';
+import {Nav, Platform, AlertController} from 'ionic-angular';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Angular2TokenService} from 'angular2-token';
 
-import { HomePage } from '../pages/home/home';
+import {HomePage} from '../pages/home/home';
 
 @Component({
   templateUrl: 'app.html'
@@ -14,22 +14,22 @@ export class MyApp {
 
   rootPage: any = HomePage;
 
-  pages: Array<{title: string, component: any}>;
+  pages: Array<{ title: string, component: any }>;
+  currentUser: any;
 
   constructor(public platform: Platform,
               public statusBar: StatusBar,
               public splashScreen: SplashScreen,
-              private _tokenService: Angular2TokenService
-  ) {
+              private _tokenService: Angular2TokenService) {
     this._tokenService.init({
-      apiBase: 'https://your-cooper-api.herokuapp.com/api/v1'
+      apiBase: 'https://ne-cooper-api.herokuapp.com//api/v1'
     });
 
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
+      {title: 'Home', component: HomePage},
     ];
 
   }
@@ -41,6 +41,55 @@ export class MyApp {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  loginPopUp() {
+    console.log('popup');
+    let confirm = this.alertCtrl.create({
+      title: 'Login',
+      inputs: [
+        {
+          name: 'email',
+          placeholder: 'email'
+        },
+        {
+          name: 'password',
+          placeholder: 'password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Login',
+          handler: data => {
+            this.login(data);
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+  login(credentials) {
+    this.tokenService
+      .signIn(credentials)
+      .subscribe(
+        res => (this.currentUser = res.json().data),
+        err => console.error('error')
+      );
+  }
+
+  logout() {
+    this.tokenService
+      .signOut()
+      .subscribe(res => console.log(res), err => console.error('error'));
+    this.currentUser = undefined;
   }
 
   openPage(page) {
