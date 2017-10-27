f82fb1a2602f2bffa42b0f87fabe7dd75adbd037
nurlanemir <nurlan.emiroff@gmail.com>
2017-10-27 12:52:32 +0200

    Removes list page, adds ionic cordova, adds home view

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  user: any = {};
 
   constructor(public navCtrl: NavController) {
-
+    this.user = {distance: 1000, age: 20};
   }
 
+  calculate() {
+    console.log(this.user);
+  }
 }
